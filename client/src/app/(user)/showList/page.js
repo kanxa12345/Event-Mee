4d5ca4002a3ede8d8f04f0e23bd5ab6d86fb5ca3
Sandip <c.restsandip123@gmail.com>
2024-04-04 14:05:27 +0545

    changes on user profile modal

---
+++ client/src/app/(user)/showList/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShowList = () => {
+  return <div>Hello I am show</div>;
+};
+
+export default ShowList;
--- client/src/components/header/page.js
+++ client/src/components/header/page.js
@@ -4,8 +4,14 @@ import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { FaRegUserCircle } from "react-icons/fa";
 import { useRouter } from "next/navigation";
+import { FaRegUserCircle, FaRegUser } from "react-icons/fa";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { MdOutlineShoppingCart, MdOutlineSlideshow } from "react-icons/md";
+import {
+  IoSettingsOutline,
+  IoChatbubbleEllipsesOutline,
+} from "react-icons/io5";
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -46,38 +52,13 @@ const Header = () => {
             <div>
               {isLogin ? (
                 <div
-                  onClick={() => setOpenModal(!openModal)}
+                  onClick={() => setOpenModal(true)}
                   className="relative flex items-center gap-1 cursor-pointer font-medium text-lg"
                 >
                   <FaRegUserCircle className="text-3xl" />
                   <p className="capitalize">
                     {userDetail?.fullName?.split(" ")[0]}
                   </p>
-                  {openModal && (
-                    <div className="absolute top-10 left-0 bg-gray-50 w-56 p-4 flex flex-col items-start gap-3 shadow-lg">
-                      <div className="flex flex-col items-start">
-                        <p className="capitalize">{userDetail.fullName}</p>
-                        <small className="text-sm font-medium text-gray-600">
-                          {userDetail.email}
-                        </small>
-                      </div>
-                      <Link href="/profile" className="inline-block w-full">
-                        Your Profile
-                      </Link>
-                      <Link href="#" className="inline-block w-full">
-                        Your Show
-                      </Link>
-                      <button
-                        onClick={() => {
-                          dispatch(logoutUser());
-                          router.push("/");
-                        }}
-                        className="w-full text-start"
-                      >
-                        Log out
-                      </button>
-                    </div>
-                  )}
                 </div>
               ) : (
                 <Link
@@ -91,6 +72,88 @@ const Header = () => {
           </div>
         </div>
       </header>
+      <div
+        className={`${
+          openModal ? "right-0" : "-right-80"
+        } fixed h-screen top-0 bg-gray-50 w-80 z-[60] py-10 flex flex-col justify-between items-start gap-3 shadow-lg transition-all duration-300 ease-linear`}
+      >
+        <div className="flex flex-col gap-4 w-full">
+          <div className="flex flex-col items-start px-4">
+            <p className="capitalize text-xl font-medium">
+              {userDetail.fullName}
+            </p>
+            <small className="text-sm font-medium text-gray-600">
+              {userDetail.email}
+            </small>
+          </div>
+          <div className="flex flex-col w-full">
+            <Link
+              onClick={() => setOpenModal(false)}
+              href="#"
+              className="flex items-center gap-3 w-full px-4 border-b border-gray-400 py-3 hover:bg-gray-200"
+            >
+              <IoIosNotificationsOutline className="text-xl" />
+              Notifications
+            </Link>
+            <Link
+              onClick={() => setOpenModal(false)}
+              href="/profile"
+              className="flex items-center gap-3 w-full px-4 border-b border-gray-400 py-3 hover:bg-gray-200"
+            >
+              <FaRegUser className="text-xl" />
+              Your Profile
+            </Link>
+            <Link
+              onClick={() => setOpenModal(false)}
+              href="/showList"
+              className="flex items-center gap-3 w-full px-4 border-b border-gray-400 py-3 hover:bg-gray-200"
+            >
+              <MdOutlineSlideshow className="text-xl" />
+              Your Show
+            </Link>
+            <Link
+              onClick={() => setOpenModal(false)}
+              href="#"
+              className="flex items-center gap-3 w-full px-4 border-b border-gray-400 py-3 hover:bg-gray-200"
+            >
+              <MdOutlineShoppingCart className="text-xl" />
+              Your Orders
+            </Link>
+            <Link
+              onClick={() => setOpenModal(false)}
+              href="#"
+              className="flex items-center gap-3 w-full px-4 border-b border-gray-400 py-3 hover:bg-gray-200"
+            >
+              <IoChatbubbleEllipsesOutline className="text-xl" />
+              Help & Support
+            </Link>
+            <Link
+              onClick={() => setOpenModal(false)}
+              href="#"
+              className="flex items-center gap-3 w-full px-4 border-b border-gray-400 py-3 hover:bg-gray-200"
+            >
+              <IoSettingsOutline className="text-xl" />
+              Account Settings
+            </Link>
+          </div>
+        </div>
+        <button
+          onClick={() => {
+            dispatch(logoutUser());
+            router.push("/");
+            setOpenModal(false);
+          }}
+          className="w-11/12 text-center py-2 bg-red-700 text-white mx-auto"
+        >
+          Log out
+        </button>
+      </div>
+      {openModal && (
+        <div
+          onClick={() => setOpenModal(false)}
+          className="fixed w-full h-full bg-gray-950 z-50 inset-0 opacity-50"
+        ></div>
+      )}
     </>
   );
 };
