7924383afe241d924f2757271e69d1550aa9635d
Sandip <c.restsandip123@gmail.com>
2024-04-04 14:39:56 +0545

    created admin layout and page to display show list & add show in user layout

---
+++ client/src/app/(user)/show/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <section className="py-24 bg-gray-50">
+        <div className="container">Hello I am show</div>
+      </section>
+    </>
+  );
+};
+
+export default page;
--- client/src/app/(user)/showList/page.js
+++ client/src/app/(user)/showList/page.js
@@ -1,7 +1,22 @@
+import Link from "next/link";
 import React from "react";
 
 const ShowList = () => {
-  return <div>Hello I am show</div>;
+  return (
+    <>
+      <section className="py-24 bg-gray-50">
+        <div className="container flex flex-col items-center gap-2">
+          <h2 className="text-4xl font-semibold">No shows available</h2>
+          <Link
+            href="/show"
+            className="inline-block py-1 px-2 bg-red-700 text-white"
+          >
+            List Your Show
+          </Link>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default ShowList;
